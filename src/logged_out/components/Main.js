6c20e034dd0f347ba13524eccace9aba81b380b5
Vlad <nvlad717@gmail.com>
2023-08-28 01:53:08 -0400

    Added text

--- src/logged_out/components/Main.js
+++ src/logged_out/components/Main.js
@@ -32,13 +32,13 @@ function Main(props) {
   const selectHome = useCallback(() => {
     smoothScrollTop();
     document.title =
-      "WaVer - Free template for building a SaaS or admin application";
+      "iDruid - Smartest indoor gardens";
     setSelectedTab("Home");
   }, [setSelectedTab]);
 
   const selectBlog = useCallback(() => {
     smoothScrollTop();
-    document.title = "WaVer - Blog";
+    document.title = "iDruid - Blog";
     setSelectedTab("Blog");
   }, [setSelectedTab]);
 
